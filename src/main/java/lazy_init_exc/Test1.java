889470408369795d51396c4f1a8d2794c3d92044
Nataly <nata.tereshkova@gmail.com>
2022-07-07 18:29:51 +0200

    add lazy init exc add n + 1 problem

---
+++ src/main/java/lazy_init_exc/Test1.java
@@ -0,0 +1,58 @@
+package lazy_init_exc;
+
+import lazy_init_exc.Entity.Department;
+import lazy_init_exc.Entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Department.class)
+                .buildSessionFactory();
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+
+//            Department dep = new Department("Sales", 800, 1500);
+//            Employee emp1 = new Employee("Artem", "Ivanov", 800);
+//            Employee emp2 = new Employee("Elena", "Smirnova", 1500);
+//            Employee emp3 = new Employee("Anton", "Sidorov", 1200);
+//
+//            session.beginTransaction();
+//
+//            dep.addEmployeeToDepartment(emp1);
+//            dep.addEmployeeToDepartment(emp2);
+//            dep.addEmployeeToDepartment(emp3);
+//
+//
+//            session.save(dep);
+//            session.getTransaction().commit();
+//            System.out.println("Done");
+
+
+//            session.beginTransaction();
+//            Department department = session.get(Department.class, 1);
+//            session.getTransaction().commit();
+//            System.out.println(department);
+//            System.out.println(department.getEmps());
+//            System.out.println("Done");
+
+            session.beginTransaction();
+            Department department = session.get(Department.class, 1);
+            System.out.println(department);
+            System.out.println(department.getEmps());
+            System.out.println("Done");
+            session.getTransaction().commit();
+
+        }
+        finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
---
+++ src/main/java/n_plus_one_problem/Test1.java
@@ -0,0 +1,46 @@
+package n_plus_one_problem;
+
+import lazy_init_exc.Entity.Department;
+import lazy_init_exc.Entity.Employee;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Expression;
+
+import java.util.List;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(lazy_init_exc.Entity.Department.class)
+                .buildSessionFactory();
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+
+//            session.beginTransaction();
+//            List list = session.createQuery("select d from departments d").getResultList();
+//            for(Object object : list) {
+//                Department department = (Department) object;
+//                System.out.println(department.getDepartmentName() + " " + department.getEmps());
+//            }
+//            session.getTransaction().commit();
+
+            session.beginTransaction();
+            List list = session.createQuery("select d from departments d join fetch d.emps e").getResultList();
+            for(Object object : list) {
+                Department department = (Department) object;
+                System.out.println(department.getDepartmentName() + " " + department.getEmps());
+            }
+            session.getTransaction().commit();
+        }
+        finally {
+            session.close();
+            factory.close();
+        }
+        }
+    }
